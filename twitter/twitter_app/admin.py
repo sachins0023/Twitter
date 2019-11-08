7ea2e34540973f1e12d3db96776d8097a68d0723
sachinsuresh <sachinsuresh00@gmail.com>
2019-11-08 17:28:17 +0530

    admin created

--- twitter/twitter_app/admin.py
+++ twitter/twitter_app/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from twitter_app.models import User, Tweet
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'name', )
+    ordering = ('name',)
+    search_fields = ['name',]
+    
+admin.site.register(User,UserAdmin)
+
+class TweetAdmin(admin.ModelAdmin):
+    list_display = ('short_text', 'user', 'text', 'date_time_created', 'edited', 'date_time_updated', 'deleted',)
+    list_filter = ('edited', 'deleted',)
+    ordering = ('date_time_updated',)
+    search_fields = ['user', 'text',]
+    
+admin.site.register(Tweet, TweetAdmin)
