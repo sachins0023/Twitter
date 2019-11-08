45d2b3cf89e54f05b529e533065e24cd2072eb69
sachinsuresh <sachinsuresh00@gmail.com>
2019-11-08 17:01:00 +0530

    models edited and testing done

--- twitter/twitter_app/models.py
+++ twitter/twitter_app/models.py
@@ -5,8 +5,8 @@ import pytz
 # Create your models here.
 
 class User(models.Model):
-    email = models.EmailField(_(""), max_length=254, primary_key=True)
-    name = models.CharField(_(""), max_length=50)
+    email = models.EmailField(max_length=254, primary_key=True)
+    name = models.CharField(max_length=50)
     
     def __str__(self):
         return self.name
@@ -14,8 +14,8 @@ class User(models.Model):
 class Tweet(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.CharField(max_length=140)
-    date_time_created = models.DateTimeField(auto_now_add=True)
-    date_time_updated = models.DateTimeField(auto_now=True)
+    date_time_created = models.DateTimeField(auto_now_add=True) #Background field
+    date_time_updated = models.DateTimeField(auto_now=True)     #Background field
     deleted = models.BooleanField(default = False)
     
     def edited(self):
@@ -23,8 +23,12 @@ class Tweet(models.Model):
             return True
         else:
             return False
+        
+    def get_short_text(self):
+        short_text = self.text[:10]+'...'
+        return short_text
     
     def __str__(self):
-        return self.text[:10]+'...'
+        return self.get_short_text()
     
     
